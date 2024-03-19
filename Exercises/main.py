55fe77ed3e3800689727d0f37a2b53630b0dac0a
Nic Taehong Kim <ktae.nic@gmail.com>
2024-03-19 22:23:57 +0900

    #1 Test: github issue test commit

--- Exercises/main.py
+++ Exercises/main.py
@@ -1,2 +1,13 @@
+from datetime import datetime, timedelta
+
+
+def get_current_time_str():
+    datetime_format_str = '%y-%m-%d %H:%M:%S.%f'
+    now_kst = datetime.utcnow() + timedelta(hours=9)
+    current_time = now_kst.strftime(datetime_format_str)
+    return current_time
+
+
 if __name__ == '__main__':
     print('Hello World!')
+    print(f'current time(KST): {get_current_time_str()}')
